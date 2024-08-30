0ba1f5dd5e47162e04523287acb9a06df6f63741
Inacio Salvador <inaciosalvador03@gmail.com>
2024-08-30 10:45:24 -0300

    Teste deveRetornarFalsoSeNaoExisteEmail() finalizado.

--- src/main/java/com/salvador/minhasfinancas/model/repository/UsuarioRepository.java
+++ src/main/java/com/salvador/minhasfinancas/model/repository/UsuarioRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    //Usando esta convenção para o nome, a JPA reconhece o metodo para verificar se o email existe
+    //Usando esta convenção para o nome, o JPA reconhece o metodo para verificar se o email existe
     //no banco de dados, inclusive reconhecendo a coluna email.
     //Pode-se usar esta convenção para verificar qualquer coluna, alterando apenas o nome do metodo
     // por exemplo existByNome()
--- src/test/java/com/salvador/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/salvador/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -4,31 +4,49 @@ import com.salvador.minhasfinancas.model.entity.Usuario;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 // 1º teste de integração, pois o teste depende de um componente externo
 // neste caso, o banco de dados postgres
 public class UsuarioRepositoryTest {
 
-    @Autowired // injetanco a intancia dessa interface
+    @Autowired // injetando a intancia dessa interface
     UsuarioRepository repository;
 
     @Test
     //Todos os metodos de test tem return void
     public void deveVerificarAExistenciaDdeUmEmail() {
-        // Para o testar a quary method existByEmail precisamos de 3 elementos que são os seguintes:
+        // Para o testar a query method existByEmail precisamos de 3 elementos que são os seguintes:
 
         // 1º Cenario
         Usuario usuario = Usuario.builder().nome("Inacio").email("inacio@email.com").build(); //
         repository.save(usuario);
 
-        // 2º Ação & execução
+        // 2º Ação ou execução
         boolean result = repository.existsByEmail("inacio@email.com");
 
         // 3º Verificação
         Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void deveRetornarFalsoSeNaoExisteEmail(){
+        // 1º cenario
+        repository.deleteAll();
+
+        // 2º Ação ou execução
+        boolean result = repository.existsByEmail("inacio@email.com");
+
+        // 3º Verificação
+        Assertions.assertFalse(result);
+
     }
 
 }
