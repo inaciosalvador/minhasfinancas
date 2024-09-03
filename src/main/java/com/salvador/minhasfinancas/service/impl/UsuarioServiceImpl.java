11674953b3af7c8fded8a010b4588787fc026d4a
Inacio Salvador <inaciosalvador03@gmail.com>
2024-09-02 21:50:28 -0300

    REFATORAR TESTES em andamento.

--- src/main/java/com/salvador/minhasfinancas/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/salvador/minhasfinancas/service/impl/UsuarioServiceImpl.java
@@ -23,7 +23,7 @@ public class UsuarioServiceImpl implements UsuarioService {
     public Usuario autenticar(String email, String senha) {
         Optional<Usuario> usuario = repository.findByEmail(email);
 
-        if (!usuario.isPresent()) {
+        if (usuario.isPresent()) {
             throw new ErroAutenticacao("Usuario não encontrado para o email informado");
         }
 
--- src/test/java/com/salvador/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/salvador/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -6,19 +6,26 @@ import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
+@DataJpaTest // Cria uma instancia de db em memoria (temporaria)
 // 1º teste de integração, pois o teste depende de um componente externo
 // neste caso, o banco de dados postgres
 public class UsuarioRepositoryTest {
 
     @Autowired // injetando a intancia dessa interface
     UsuarioRepository repository;
+
+    @Autowired
+    TestEntityManager entityManager; // Contem operações de CRUD na base de dados em teste
 
     @Test
     //Todos os metodos de test tem return void
@@ -27,7 +34,7 @@ public class UsuarioRepositoryTest {
 
         // 1º Cenario
         Usuario usuario = Usuario.builder().nome("Inacio").email("inacio@email.com").build(); //
-        repository.save(usuario);
+        entityManager.persist(usuario);
 
         // 2º Ação ou execução
         boolean result = repository.existsByEmail("inacio@email.com");
@@ -39,7 +46,7 @@ public class UsuarioRepositoryTest {
     @Test
     public void deveRetornarFalsoSeNaoExisteEmail(){
         // 1º cenario
-        repository.deleteAll();
+        // repository.deleteAll(); Não utiliza-se mais.
 
         // 2º Ação ou execução
         boolean result = repository.existsByEmail("inacio@email.com");
